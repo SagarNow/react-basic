42484d38c2cf9d9328de613d04ce959eeaf534b1
sagar singh <sagarsingh86199@gmail.com>
2025-04-26 22:07:15 +0530

    hover

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -36,13 +36,13 @@ const [c, d] = useState(0);
       <div className=" box1 flex flex-col gap-8">
         <div
           className=" usestate h-20 w-25 bg-purple-300 
-  rounded-2xl shadow-lg px-4 py-2 flex justify-center items-center"
-        >
+  rounded-2xl shadow-lg px-4 py-2 flex justify-center items-center hover:bg-amber-400"
+         >
           {a}
         </div>
 
         <button
-          className="h-8 w-25 bg-purple-600 hover:bg-purple-400 rounded-md shadow-lg px-4 py-2 flex justify-center items-center text-white"
+          className="h-8 w-25 bg-purple-600 hover:bg-purple-400 rounded-md shadow-lg px-4 py-2 flex justify-center items-center text-white "
           onClick={() => {
             b(a + 1);
           }}
@@ -56,7 +56,7 @@ const [c, d] = useState(0);
 <div className=" flex flex-col gap-8 ">
 <div
           className=" usestate h-20 w-25 bg-purple-300 
-  rounded-2xl shadow-lg px-4 py-2 flex justify-center items-center"
+  rounded-2xl shadow-lg px-4 py-2 flex justify-center items-center hover:bg-amber-400"
         >
           {c}
         </div>
